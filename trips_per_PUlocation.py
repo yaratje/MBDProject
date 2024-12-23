20a593f1abc61703831f099516b67a7e36ca215d
GerwinHunter <gerwin@de-jager.info>
2024-12-23 11:34:23 +0100

    Changed trips_per_PUlocation.py so it generates seperate files for the years 2019 till 2024

--- trips_per_PUlocation.py
+++ trips_per_PUlocation.py
@@ -6,53 +6,61 @@ from pyspark.sql.functions import column as col, desc
 
 spark = SparkSession.builder.getOrCreate()
 
-# Define general file path
-filePath = "/user/s2645963/project/processed_data/2024/"
-
-# Create dataframe per
-df_fhv = spark.read.parquet(filePath + "fhv_*.parquet") \
-    .select(col("PULocationID")) \
-    .groupBy("PULocationID") \
-    .count() \
-    .withColumnRenamed("count", "fhv_trips")
-
-df_fhvhv = spark.read.parquet(filePath + "fhvhv_*.parquet") \
-    .select(col("PULocationID")) \
-    .groupBy("PULocationID") \
-    .count() \
-    .withColumnRenamed("count", "fhvhv_trips")
-
-df_green = spark.read.parquet(filePath + "green_*.parquet") \
-    .select(col("PULocationID")) \
-    .groupBy("PULocationID") \
-    .count() \
-    .withColumnRenamed("count", "green_trips")
-
-df_yellow = spark.read.parquet(filePath + "yellow_*.parquet") \
-    .select(col("PULocationID")) \
-    .groupBy("PULocationID") \
-    .count() \
-    .withColumnRenamed("count", "yellow_trips")
-
-# Combine the dataframes in to one with output being:
-# Key = locationID, column = types and data = summed total of rides
-joined_df = df_fhv.join(df_fhvhv, on="PULocationID", how="inner") \
-    .join(df_green, on="PULocationID", how="inner") \
-    .join(df_yellow, on="PULocationID", how="inner") \
-
-joined_df.show(265)
-
-# Add joined dataframe to the location data csv
+# Import the location description csv
 df_location_import = spark.read.csv("/user/s2645963/project/taxi_zone_lookup.csv", header="true", inferSchema="true")
-df_location = df_location_import.select(col("LocationID").alias("PULocationID"), col("Borough"), col("Zone"))
 
-# Join joined and location dataframe
-df_print = joined_df.join(df_location, on="PULocationID", how="inner") \
-    .select(col("PULocationID"), col("Borough"), col("Zone"), col("fhv_trips"), col("fhvhv_trips"), col("green_trips"), col("yellow_trips")) \
-    .orderBy("PULocationID")
-df_print.show(265)
+# Define general file path
+filePath = "/user/s2645963/project/processed_data/"
+years = [f"{y:02d}" for y in range(19, 25)]
 
-# Write the dataframe to csv on HDFS
-output = "/user/s2645963/project/output/" + "Trips_per_PULocationID"
-df_print.write.mode("overwrite").csv(output)
+for year in years:
+    current_year = "20" + year + "/"
+    year_path = filePath + current_year
+
+    # Create dataframe per
+    df_fhv = spark.read.parquet(year_path + "fhv_*.parquet") \
+        .select(col("PULocationID")) \
+        .groupBy("PULocationID") \
+        .count() \
+        .withColumnRenamed("count", "fhv_trips")
+
+    df_fhvhv = spark.read.parquet(year_path + "fhvhv_*.parquet") \
+        .select(col("PULocationID")) \
+        .groupBy("PULocationID") \
+        .count() \
+        .withColumnRenamed("count", "fhvhv_trips")
+
+    df_green = spark.read.parquet(year_path + "green_*.parquet") \
+        .select(col("PULocationID")) \
+        .groupBy("PULocationID") \
+        .count() \
+        .withColumnRenamed("count", "green_trips")
+
+    df_yellow = spark.read.parquet(year_path + "yellow_*.parquet") \
+        .select(col("PULocationID")) \
+        .groupBy("PULocationID") \
+        .count() \
+        .withColumnRenamed("count", "yellow_trips")
+
+    # Combine the dataframes in to one with output being:
+    # Key = locationID, column = types and data = summed total of rides
+    joined_df = df_fhv.join(df_fhvhv, on="PULocationID", how="inner") \
+        .join(df_green, on="PULocationID", how="inner") \
+        .join(df_yellow, on="PULocationID", how="inner") \
+
+    # joined_df.show(265)
+
+    # Take the location, burough and zone columns from the import csv
+    df_location = df_location_import.select(col("LocationID").alias("PULocationID"), col("Borough"), col("Zone"))
+
+    # Join joined and location dataframe
+    df_print = joined_df.join(df_location, on="PULocationID", how="inner") \
+        .select(col("PULocationID"), col("Borough"), col("Zone"), col("fhv_trips"), col("fhvhv_trips"), col("green_trips"), col("yellow_trips")) \
+        .orderBy("PULocationID")
+    # df_print.show(265)
+
+    # Write the dataframe to csv on HDFS
+    output = "/user/s2645963/project/output/" + "Trips_per_PULocationID/" + current_year
+    df_print.write.mode("overwrite").csv(output)
+    print("Saved output for the year " + current_year + " to file path " + output)
 
