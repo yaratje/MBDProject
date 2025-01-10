c07560371082c39e6e1500b8fb3712502ba0802c
yaratje <yaratje48@gmail.com>
2025-01-10 15:43:58 +0100

    airport part done (ish)

---
+++ geoToLongLat.py
@@ -0,0 +1,15 @@
+import geopandas as gpd
+
+# Load the shapefile
+shapefile_path = "./zoneShape/taxi_zones.shp"
+gdf = gpd.read_file(shapefile_path)
+gdf = gdf.to_crs(epsg=4326)
+print(gdf.crs)
+
+# Extract centroids for polygons or use existing points
+gdf["Longitude"] = gdf.geometry.centroid.x
+gdf["Latitude"] = gdf.geometry.centroid.y
+
+# Save as a CSV to use in Tableau
+output_path = "output_with_lat_lon.csv"
+gdf[["LocationID", "Longitude", "Latitude"]].to_csv(output_path, index=False)
